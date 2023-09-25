081f7eef552cbadeb66a682bfcd2fb4acda546dd
mike <mstralexandre@gmail.com>
2023-09-25 14:02:16 -0400

    nav links in, adding styling

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,6 +6,26 @@ export default function Header() {
   return (
     <nav>
       <Image src={Logo} alt="logo" />
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">About</Link>
+        </li>
+        <li>
+          <Link href="/">Menu</Link>
+        </li>
+        <li>
+          <Link href="/reservations">Reservations</Link>
+        </li>
+        <li>
+          <Link href="/">Order Online</Link>
+        </li>
+        <li>
+          <Link href="/">Login</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
