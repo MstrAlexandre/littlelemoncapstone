f8432424b18db550fbfe0794b4cce3ed0e84fee7
mike <mstralexandre@gmail.com>
2023-09-26 10:13:43 -0400

    starting hero section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,24 @@
 import Image from "next/image";
+import Restaurant from "public/restaurant.jpg";
 
 export default function Home() {
   return (
     <main>
+      <div className="hero">
+        <div>
+          <h3 className="lead">Little Lemon</h3>
+          <p>Chicago</p>
+        </div>
+        <div>
+          <Image
+            src={Restaurant}
+            alt="Inside the Restaurant"
+            width={300}
+            height={300}
+            id="heropic"
+          />
+        </div>
+      </div>
       <h1>Hello World, this will soon be the home page content</h1>
     </main>
   );
